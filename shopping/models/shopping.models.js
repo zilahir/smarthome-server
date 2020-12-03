29a8d882f52e22195b6deb14bfbe812434c8fa67
Richard Zilahi <richard.zilahi@salesframe.com>
2020-12-03 08:49:14 +0200

    implementing shopping API

--- shopping/models/shopping.models.js
+++ shopping/models/shopping.models.js
@@ -15,7 +15,6 @@ const shoppingSchema = new Schema({
 const productSchema = new Schema({
   productId: String,
   productName: String,
-  productPrice: Number,
 })
 
 const ShoppingItem = mongoose.model('Shopping', shoppingSchema)
